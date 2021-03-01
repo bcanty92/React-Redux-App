b10cff1991447bd63976729fcd4676067f00c0d2
BrittanyCanty <bcanty92@gmail.com>
2021-03-01 16:55:35 -0600

    made new app

--- app/src/index.js
+++ app/src/index.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from "redux";
-import { Provider } from "react-redux";
-import thunk from "redux-thunk";
-import { pokemonReducer as reducer } from "./reducers/reducers";
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.min.css';
-const store = createStore(reducer, applyMiddleware(thunk))
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+import {createStore, applyMiddleware} from 'redux';
+import {jokesReducer} from './reducers/jokesReducer'
+import {Provider} from 'react-redux'
+import thunk from 'redux-thunk';
+
+const store = createStore(jokesReducer, applyMiddleware(thunk))
+
+ReactDOM.render(
+  <React.StrictMode>
+  <Provider store={store}>
+    <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
